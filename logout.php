bf6f1ba20383c0ce2517e111d67723e9f5c49bd7
Joseph Pacura <jpacura96@gmail.com>
2017-10-17 14:50:12 -0400

    Converted all HTML documents from XHTML 1.0 to HTML 5

--- logout.php
+++ logout.php
@@ -8,9 +8,8 @@
 	IT WORKS, DO NOT BREAK IT!
 -->
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">  <!-- REQUIRED HTML HEADER -->
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en"> <!-- REQUIRED HTML HEADER -->
+<!DOCTYPE html>
+<html lang="en">
 	<head>
 		<title>Logging Out...</title>
 	</head>
@@ -27,3 +26,4 @@
 			document.ready(window.setTimeout(location.href = "login.php",2000));
 		</script>
 	</body>
+</html>
