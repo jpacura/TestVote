98d67f16c9d938f087c0b4d308308b23d176dbda
Joseph Pacura <jpacura96@gmail.com>
2017-10-22 17:27:35 -0400

    Fixed registerschool

--- angular/controllers/schoolController.js
+++ angular/controllers/schoolController.js
@@ -48,6 +48,27 @@ myApp.controller('schoolController', ['$scope', '$http', function ($scope, $http
                     if(response.data.error)
                     {
 						// THERE IS AN ERROR
+						
+						var errout = "ERROR: UNKNOWN SERVER ERROR!";
+						if(response.data.errorcode == 5)
+						{
+							// NOT LOGGED IN
+							errout = "ERROR: NOT LOGGED IN!";
+							window.location.href = "logout.php";
+						}
+						else if(response.data.errorcode == 4)
+						{
+							// SCHOOL ALREADY EXISTS
+							errout = "This School Already Exists!";
+						}
+						
+						$scope.errtext = errout;
+						$scope.isNotEnrolled = true;
+					}
+					else
+					{
+						// NO ERRORS
+					}
                 },
                 function errorCallback(response) {
                     console.log(response.statusText);
